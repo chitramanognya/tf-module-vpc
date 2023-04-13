834aa0bed0e50c878bb4d84566bbd37a7ac8a010
chitra <chmanu.chinu@gmail.com>
2023-04-13 10:10:56 +0000

    update tf-module-vpc

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 }
 
 # Peering
-resource "aws_vpc_peering_connecion" "peer" {
+resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.account.account_id
   peer_vpc_id = var.default_vpc_id
   vpc_id = aws_vpc.main.id 
