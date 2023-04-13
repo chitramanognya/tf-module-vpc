a70603e917638428af8ac0d11d380638c1e797a1
chitra <chmanu.chinu@gmail.com>
2023-04-13 08:13:35 +0000

    update tf-module-vpc

--- main.tf
+++ main.tf
@@ -20,16 +20,38 @@ resource "aws_subnet" "public_subnets" {
   
 }
 
-## private subnets
-resource "aws_subnet" "private_subnets" {
+## public route table
+resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
-  tags = merge(
+
+   for_each = var.public_subnets
+    tags = merge(
     var.tags, 
       { Name = "${var.env}-${each.value["name"]}" }
   )
   
+}
+## private subnets
+resource "aws_subnet" "private_subnets" {
+  vpc_id = aws_vpc.main.id
+
   for_each = var.private_subnets
     cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
-  
+  tags = merge(
+    var.tags, 
+      { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+
+## private route tables
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.private_subnets
+   
+  tags = merge(
+    var.tags, 
+      { Name = "${var.env}-${each.value["name"]}" }
+  )
 }
